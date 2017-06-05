50cc12e8e9db70f74c85058d96500a792280133f
oelizondo <oscarmarcelo95@gmail.com>
2017-06-04 19:38:23 -0500

    initial commit

---
+++ URLS.js
@@ -0,0 +1,13 @@
+module.exports =
+['https://en.tutiempo.net/climate/01-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/02-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/03-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/04-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/05-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/06-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/07-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/08-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/09-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/10-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/11-1977/ws-763930.html',
+ 'https://en.tutiempo.net/climate/12-1977/ws-763930.html']
---
+++ index.js
@@ -0,0 +1,80 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const fs      = require('fs')
+
+const URLS = require('./URLS')
+
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (error) reject(error)
+      resolve(html)
+    })
+  })
+}
+
+const filterTableRows = (table) => {
+  return new Promise((resolve, reject) => {
+    let row = null
+    let values = []
+    for (let i = 0; i <= 9; i++) {
+      row = table.children[i]
+      if (typeof row === 'undefined')
+        continue
+      values.push(row.children[0]['data'])
+    }
+    resolve(values)
+    reject('Error')
+  })
+}
+
+const getTable = (html) => {
+  return new Promise((resolve, reject) => {
+    const $ = cheerio.load(html)
+    let table = $('table').children()
+    resolve(table['3'])
+    reject('Error')
+  })
+}
+
+const extractData = (table) => {
+  return new Promise((resolve, reject) => {
+    let csv = []
+    table.children.map(async (tr, index) => {
+      let cleanedRow = null
+      if (index != 0 && (index != 32 || index != 33)) {
+        cleanedRow = await filterTableRows(tr)
+        csv.push(cleanedRow)
+      }
+    })
+    resolve(csv)
+    reject('Error')
+  })
+}
+
+const spliceDataset = (data) => {
+  return new Promise((resolve, reject) => {
+    let flatData = []
+    data.map((row) => row.map((data) =>  flatData.push(data)))
+    for (let i = 9; i < flatData.length; i+=10)
+      flatData.splice(i, 1, '\n')
+
+    resolve(flatData)
+    reject('Error')
+  })
+}
+
+const writeToFile = (data) => {
+  fs.appendFile('data.csv', data, (error) => {
+    if (error) console.log(error)
+    console.log('Done')
+  })
+}
+
+URLS.map((url) => {
+  get(url).then(getTable)
+          .then(extractData)
+          .then(spliceDataset)
+          .then(writeToFile)
+          .catch((error) => console.log(error))
+})
